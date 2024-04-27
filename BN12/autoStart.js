cc7fbd90f62fe1870bea1481a1f68c4bc0674dfb
Tony <41136929+Goodwill82@users.noreply.github.com>
2024-04-26 18:14:06 -0700

    Create autoStart.js

---
+++ BN12/autoStart.js
@@ -0,0 +1,7 @@
+// autoStart.js is to figure out what we have (e.g. what portopeners/factions already available, or bitnodes were unlocked - or additions or APIs they provide). 
+// once we know what we have, start up a looping script (which one should be taylored to what we have. 
+
+// this script should run no matter when run:
+// start of bitnode
+// after aug install
+// after reopening for close or crash
